b27a277eaa5d099abebc36b09538bffc6b5fc8fa
Kevin McBride <30584947+tallmega@users.noreply.github.com>
2017-11-06 15:20:24 -0500

    Update InactiveUsers.ps1

--- InactiveUsers.ps1
+++ InactiveUsers.ps1
@@ -1,53 +1,57 @@
-﻿import-module activedirectory
-#This script will disable inactive users, and log which accounts were disabled.
-#This script was written to ensure that we are meeting an information security metric.
-#The metric requires that accounts that have not been used in 45 days be disabled.
-#The script runs against x OU, y OU, and for the time being will exclude Mailboxes and Meeting Rooms.
-#Theres really no reason to keep those enabled - but management wanted caution.
+import-module activedirectory
+# This script will disable inactive users, service accounts and log which accounts were disabled.
+# It also clears the passwordneverexpires flag.
+# This script was written to ensure that we are meeting an information security metric.
+# The metric requires that user accounts that have not been used in 45 days be disabled, and service accounts not used in 2 years be disabled.
 
-#Specifically it excludes the following OUs:
-#   domain.local/x/No User Policies/Mailbox Accounts
-#   domain.local/x/No User Policies/Meeting Rooms
-#   domain.local/y/Distribution Lists
-#
-# Script written by Kevin McBride - @tallmega
+# The script runs against all OUs unless specified.
+# Theres really no reason to exclude any but management wanted caution and I thought exclusion examples might be useful to some.
+
+# In order to automate this, just set it as a scheduled task:
+#   Choose a User with permission to disable accounts
+#   Select Run whether the user is logged in or not.
+#   Under actions, choose 'New' with the following options:
+#   Action: Start a Program
+#   Program/Script: Powershell
+#   Add Arguments: C:\CustomScripts\InactiveUsers45.ps1 >> C:\CustomScripts\InactiveUsers.log
+#   Then just set it to run as often as you need to.
+# Written by Kevin McBride (@tallmega) with lots of help from The Internet.
+
 date
 echo ""
-echo "DISABLING X ACCOUNTS THAT HAVE BEEN USED, BUT NOT FOR 45 DAYS:"
-search-adaccount -accountinactive -searchbase "OU=x,DC=domain,DC=local" -usersonly -timespan "45" | 
+echo "DISABLING USER ACCOUNTS THAT HAVE BEEN USED, BUT NOT FOR 45 DAYS:"
+search-adaccount -accountinactive  -usersonly -timespan "45" | 
 where Enabled -eq "True" | # looks for enabled accounts
 where LastLogonDate -notlike "" | # excludes accounts that haven't ever logged in
-Where {$_.DistinguishedName -notlike "*OU=Mailbox Accounts,OU=No User Policies,OU=x,DC=domain,DC=local"} | #excludes Mailboxes
-Where {$_.DistinguishedName -notlike "*OU=Meeting Rooms,OU=No User Policies,OU=x,DC=domain,DC=local"} | #excludes Meeting Rooms
-#Disable-ADAccount –passthru | set-aduser -Description ((get-date).toshortdatestring()) –passthru |
-select sAMAccountName, Enabled, LastLogonDate, PasswordExpired | Format-Table
+where {$_.DistinguishedName -notlike "*OU=Service Accounts,DC=domain,DC=local"} | #excludes service accounts
+where {$_.DistinguishedName -notlike "*CN=Users,DC=domain,DC=local"} | #excludes default account - !! exclusion should be removed
+Disable-ADAccount –passthru | set-aduser -Description ((get-date).toshortdatestring()) –passthru | Set-ADUser -PasswordNeverExpires:$False -passthru |
+select sAMAccountName, Enabled, LastLogonDate, PasswordExpired, distinguishedname | Format-Table
 echo " "
-echo "DISABLING X ACCOUNTS THAT HAVE NEVER BEEN USED AND ARE OLDER THAN 45 DAYS:"
+echo "DISABLING USER ACCOUNTS THAT HAVE NEVER BEEN USED AND ARE OLDER THAN 45 DAYS:"
 $ts = (get-date).AddDays(-45)
-search-adaccount -accountinactive -searchbase "OU=x,DC=domain,DC=local" -usersonly -timespan "45" | 
+search-adaccount -accountinactive -usersonly -timespan "45" | 
 where Enabled -eq "True" | # looks for enabled accounts
 where LastLogonDate -eq $null | # that haven't ever been used
-Where {$_.DistinguishedName -notlike "*OU=Mailbox Accounts,OU=No User Policies,OU=x,DC=domain,DC=local"} | #excludes Mailboxes
-Where {$_.DistinguishedName -notlike "*OU=Meeting Rooms,OU=No User Policies,OU=x,DC=domain,DC=local"} | #excludes Meeting Rooms
+where {$_.DistinguishedName -notlike "*OU=Service Accounts,DC=domain,DC=local"} | #excludes service accounts
+where {$_.DistinguishedName -notlike "*CN=Users,DC=domain,DC=local"} | #excludes default ou - !! exclusion should be removed
 get-aduser -Properties *| where whenCreated -lt $ts |
-#Disable-ADAccount –passthru | set-aduser -Description ((get-date).toshortdatestring()) –passthru | 
-select sAMAccountName, Enabled, Created, PasswordExpired | Format-Table  
+Disable-ADAccount –passthru | set-aduser -Description ((get-date).toshortdatestring()) –passthru | Set-ADUser -PasswordNeverExpires:$False -passthru |
+select sAMAccountName, Enabled, Created, PasswordExpired, distinguishedname | Format-Table  
 echo " "
-echo "DISABLING Y HAVE BEEN USED, BUT NOT FOR 45 DAYS:"
-search-adaccount -accountinactive -searchbase "OU=y,DC=domain,DC=local" -usersonly -timespan "45" | 
-where Enabled -eq "True" |
-where LastLogonDate -notlike "" |
-Where {$_.DistinguishedName -notlike "*OU=Distribution Lists,OU=y,DC=domain,DC=local"} |
-#Disable-ADAccount –passthru |set-aduser -Description ((get-date).toshortdatestring()) –passthru |
-select sAMAccountName, Enabled, LastLogonDate, PasswordExpired | Format-Table
+echo "DISABLING SERVICE ACCOUNTS THAT HAVE BEEN USED, BUT NOT FOR 2 YEARS:"
+search-adaccount -accountinactive -searchbase "OU=Service Accounts,DC=domain,DC=local" -usersonly -timespan "730" | 
+where Enabled -eq "True" | # looks for enabled accounts
+where LastLogonDate -notlike "" | # excludes accounts that haven't ever logged in
+Disable-ADAccount –passthru | set-aduser -Description ((get-date).toshortdatestring()) –passthru |
+select sAMAccountName, Enabled, LastLogonDate, PasswordExpired, distinguishedname | Format-Table
 echo " "
-echo "DISABLING Y ACCOUNTS THAT HAVE NEVER BEEN USED AND ARE OLDER THAN 45 DAYS:"
-$ts = (get-date).AddDays(-45)
-search-adaccount -accountinactive -searchbase "OU=y,DC=domain,DC=local" -usersonly -timespan "45" | 
-where Enabled -eq "True" |
-where LastLogonDate -eq $null |
-Where {$_.DistinguishedName -notlike "*OU=Distribution Lists,OU=y,DC=domain,DC=local"} | #excludes distribution list ou (mailboxes)
-get-aduser -Properties *| where whenCreated -lt $ts | 
-#Disable-ADAccount –passthru | set-aduser -Description ((get-date).toshortdatestring()) –passthru |
-select sAMAccountName, Enabled, Created, PasswordExpired | Format-Table 
+echo "DISABLING SERVICE ACCOUNTS THAT HAVE NEVER BEEN USED AND ARE OLDER THAN 2 YEARS:"
+$ts = (get-date).AddDays(-730)
+search-adaccount -accountinactive -searchbase "OU=Service Accounts,DC=domain,DC=local" -usersonly -timespan "730" | 
+where Enabled -eq "True" | # looks for enabled accounts
+where LastLogonDate -eq $null | # that haven't ever been used
+get-aduser -Properties *| where whenCreated -lt $ts | #created more than 2 years ago
+Disable-ADAccount –passthru | set-aduser -Description ((get-date).toshortdatestring()) –passthru |
+select sAMAccountName, Enabled, Created, PasswordExpired, distinguishedname | Format-Table  
 echo "Complete!"
